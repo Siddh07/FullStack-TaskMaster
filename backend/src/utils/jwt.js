f351a01d22be4e9784585b79c54c00b055d589ec
Siddhant Shrestha <sidd.hant.1793@gmail.com>
2026-02-16 23:06:14 +0545

    jwt token

---
+++ backend/src/utils/jwt.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (payload) => {
+  try {
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRE || "7d",
+    });
+
+    return token;
+  } catch (error) {
+    throw new Error("Error generating token");
+  }
+};
+
+// Function to verify JWT token
+const verifyToken = (token) => {
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    return decoded;
+  } catch (error) {
+    throw new Error("Invalid or expired token");
+  }
+};
+
+module.exports = {
+  generateToken,
+  verifyToken,
+};
